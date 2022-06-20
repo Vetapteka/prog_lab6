9b72be7bba109410f1d468418028a15fba008718
vetapteka <cvetka_top@mail.ru>
2022-06-21 00:42:47 +0300

    add insert to DatabaseManager

--- src/main/java/utils/DatabaseManager.java
+++ src/main/java/utils/DatabaseManager.java
@@ -1,9 +1,11 @@
 package utils;
 
+import model.Flat;
+
 import java.sql.*;
 import java.util.Properties;
 
-public class DatabaseHandler {
+public class DatabaseManager {
     private static final Properties properties = PropertiesManager.getProperties();
     private static final String url = properties.getProperty("urlLocal");
     private static final String username = properties.getProperty("username");
@@ -46,6 +48,47 @@ public class DatabaseHandler {
             id = resultSet.getObject(1, Integer.class);
         }
         return id;
+    }
+
+
+    public static Integer insertFlat(Flat flat, int userId) throws SQLException {
+
+        String INSERT_FLAT = "INSERT INTO flats (id, user_id, flat_name, coor_x, coor_y, init_date, area, number_of_rooms, furnish, flat_view, transport, house_name, house_age, house_floor) " +
+                "Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
+                "RETURNING id";
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_FLAT);
+
+        connection.setAutoCommit(false);
+
+        if (flat.getHouse() != null) {
+
+            preparedStatement.setString(12, flat.getHouse().getName());
+            preparedStatement.setLong(13, flat.getHouse().getYear());
+            preparedStatement.setDouble(14, flat.getHouse().getNumberOfFlatsOnFloor());
+        } else {
+            preparedStatement.setNull(12, Types.CHAR);
+            preparedStatement.setNull(13, Types.BIGINT);
+            preparedStatement.setNull(14, Types.DOUBLE);
+        }
+
+        preparedStatement.setInt(1, flat.getId());
+        preparedStatement.setInt(2, userId);
+        preparedStatement.setString(3, flat.getName());
+        preparedStatement.setFloat(4, flat.getCoordinates().getX());
+        preparedStatement.setDouble(5, flat.getCoordinates().getY());
+        preparedStatement.setTimestamp(6, Timestamp.from(flat.getCreationDate().toInstant()));
+        preparedStatement.setDouble(7, flat.getArea());
+        preparedStatement.setInt(8, flat.getNumberOfRooms());
+        preparedStatement.setInt(9, flat.getFurnish().ordinal());
+        preparedStatement.setInt(10, flat.getView().ordinal());
+        preparedStatement.setInt(11, flat.getTransport().ordinal());
+
+        preparedStatement.execute();
+
+        ResultSet last_updated_ticket = preparedStatement.getResultSet();
+        last_updated_ticket.next();
+        return last_updated_ticket.getInt(1);
+
     }
 
 }
